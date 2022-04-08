a1b6246435ace2589f4ccd71b0e7d7d2686a6975
Muskan Sagar <99795889+Muskan1283@users.noreply.github.com>
2022-04-08 11:14:47 +0530

    Add files via upload

---
+++ Swapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.WriteLine("Enter the first number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("After Swapping ");
+            Console.WriteLine("\n First number:" + num1);
+            Console.WriteLine("\n Second number:" + num2);
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
